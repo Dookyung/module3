058b391da48d9604fdea925337bea469dd7d0625
Dookyung Kim <dataexpert2017@gmail.com>
2018-03-11 18:30:26 +0900

    commit purrr

--- rscript2.R
+++ rscript2.R
@@ -1,1 +1,23 @@
 format(Sys.Date(), '%A, %B, %d, %Y')
+
+# R code for the render function
+
+rmarkdown::render("steakArticleParams.Rmd",
+                  params = list(region = "Mountain"))
+
+
+# R code to create the custom function
+
+library(purrr)
+render_report <-function(regionvar){
+    template <-"Steak_article_params_list.Rmd"
+    outfile <-sprintf("steakArticle_%s.html",regionvar)
+    parameters <-list(region = regionvar)
+    rmarkdown::render(template,
+                      output_file=outfile,
+                      params=parameters)
+    invisible(TRUE)
+}
+
+render_report("Pacific")
+
--- script1.R
+++ script1.R
@@ -11,3 +11,7 @@ ggplot(sdat,
        aes(x = steak_prep, fill = female)) + 
     geom_bar(position="dodge") +
     ggtitle(paste0("Steak Preparation Preference by Gender for ","Mountain"," Region"))
+
+
+rmarkdown::render("Steak_article_params_list.Rmd",
+                  params = list(region= 'Mountain'))
